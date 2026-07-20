15f9f29567a211bbb2a0a2c2ec68aa7cdbfb5f9e
Dhathri Chidananda Poojary <poojarydhathri@gmail.com>
2026-07-20 19:39:56 +0530

    Remove unused shlex import

--- Day02/main.py
+++ Day02/main.py
@@ -1,5 +1,3 @@
-from shlex import split
-
 print("Welcome to the tip calculator!")
 bill=float(input("What was the total bill? $"))
 tip=int(input("How much tip would you like to give?10,12 or 15? "))
